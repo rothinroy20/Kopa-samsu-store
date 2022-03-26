0839dbccb7f84ab4dd5d770fe96cc4b0dd554487
Your Name <rothinroy320@gmail.com>
2022-03-26 13:32:07 +0600

    Navbar add

---
+++ src/Navbar/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import "./Navbar.css";
+const Navbar = () => {
+    return (
+        <div>
+            <h2>kopa sumsu store</h2>
+        </div>
+    );
+};
+
+export default Navbar;
